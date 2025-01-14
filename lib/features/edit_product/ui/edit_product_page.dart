dbd30a99041159580f66266e92bcb6a7584a1b33
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-01-15 02:07:04 +0500

    code refactor

--- lib/features/edit_product/ui/edit_product_page.dart
+++ lib/features/edit_product/ui/edit_product_page.dart
@@ -1,7 +1,5 @@
 import 'package:ecommerce_admin/features/edit_product/bloc/edit_product_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class EditProductPage extends StatefulWidget {
   final String name;
